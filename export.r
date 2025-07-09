04c9cac6860cceae0f3f6eb58323f50a29fbb7db
stepangordeev <st.gordeev@gmail.com>
2025-07-08 20:17:41 -0500

    emulate gh deploy for test

--- export.r
+++ export.r
@@ -1,8 +1,7 @@
-# run for shinylive to export the app as a site
-shinylive::export(appdir = "app", destdir = "docs/")
+# run this once to create a GitHub action for deploying the app
+# usethis::use_github_action(url="https://github.com/posit-dev/r-shinylive/blob/actions-v1/examples/deploy-app.yaml")
 
-# test
-httpuv::runStaticServer("docs/")
 
-# run this to create a GitHub action for deploying the app
-usethis::use_github_action(url="https://github.com/posit-dev/r-shinylive/blob/actions-v1/examples/deploy-app.yaml")
+## emulate the gh action locally (make sure destdir is in .gitignore)
+shinylive::export(appdir = "./", destdir = "site/")
+httpuv::runStaticServer("site/")
