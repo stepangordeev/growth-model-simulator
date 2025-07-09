e3a1217191d603a7f849d3761df83152896d8a48
stepangordeev <stgordeev@gmail.com>
2025-07-09 16:01:24 -0500

    primevue test

--- js-app/src/main.js
+++ js-app/src/main.js
@@ -1,6 +1,21 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import App from './App.vue'
+import Aura from '@primeuix/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.mount("#app");
