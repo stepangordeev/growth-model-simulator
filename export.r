c1ec7bcfdd7108c0b45e10fec4c842f928416305
stepangordeev <st.gordeev@gmail.com>
2025-07-08 19:56:00 -0500

    gh action

--- export.r
+++ export.r
@@ -3,3 +3,6 @@ shinylive::export(appdir = "app", destdir = "docs/")
 
 # test
 httpuv::runStaticServer("docs/")
+
+# run this to create a GitHub action for deploying the app
+usethis::use_github_action(url="https://github.com/posit-dev/r-shinylive/blob/actions-v1/examples/deploy-app.yaml")
