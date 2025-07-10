48259863fd36eb8d842b7db33aa522a73fe7db9e
stepangordeev <stgordeev@gmail.com>
2025-07-10 14:25:29 -0500

    Revert "drop js experiments"
    This reverts commit 8a20b2e28f0b5c3a46b5561bc1e2d0ec648df7fe.

---
+++ js-app/src/App.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import Chart from 'primevue/chart';
+import Button from 'primevue/button';
+
+onMounted(() => {
+    chartData.value = setChartData();
+    chartOptions.value = setChartOptions();
+});
+
+const chartData = ref();
+const chartOptions = ref();
+        
+const setChartData = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+
+    return {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: documentStyle.getPropertyValue('--p-cyan-500'),
+                tension: 0.4
+            }
+        ]
+    };
+};
+const setChartOptions = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--p-text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+    const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+    return {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder
+                }
+            },
+            y: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder
+                }
+            }
+        }
+    };
+}
+    
+</script>
+
+<template>
+  <div class="card">
+        <Chart type="line" :data="chartData" :options="chartOptions" class="h-[30rem]" />
+  </div>
+</template>
+
+
+<style scoped>
+</style>
