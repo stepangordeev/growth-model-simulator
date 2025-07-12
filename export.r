ca2a7af35a3f93667bcf50f18d6a4cb63a372303
stepangordeev <st.gordeev@gmail.com>
2025-07-11 23:31:20 -0500

    drop R dash, promote vue folder

--- export.r
+++
@@ -1,7 +0,0 @@
-# run this once to create a GitHub action for deploying the app
-# usethis::use_github_action(url="https://github.com/posit-dev/r-shinylive/blob/actions-v1/examples/deploy-app.yaml")
-
-
-## emulate the gh action locally (make sure destdir is in .gitignore)
-shinylive::export(appdir = "./", destdir = "site/")
-httpuv::runStaticServer("site/")
