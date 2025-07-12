bc1121329c144cce60d692670aad4bb0a6d793e1
stepangordeev <st.gordeev@gmail.com>
2025-07-11 23:55:28 -0500

    draft deploy script, clean up deps more

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/macro-simulation-dash/",
   plugins: [
     vue(),
     vueDevTools(),
