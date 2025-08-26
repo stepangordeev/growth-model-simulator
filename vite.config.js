fa09f3848230854f9369b1f20e0147df785b12c7
stepangordeev <stgordeev@gmail.com>
2025-08-26 17:10:34 -0500

    upd config for new repo name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/macro-simulation-dash/",
+  base: "/growth-model-simulator/",
   plugins: [
     vue(),
     vueDevTools(),
